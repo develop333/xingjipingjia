f156533557720d5fe5d856b17ce158c0b2db0225
develop333 <45626531+develop333@users.noreply.github.com>
2019-12-09 13:32:36 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express'),
+  app = express(),
+  session = require('express-session'),
+  Mongosession = require('connect-mongo')(session),
+  mongoose = require('mongoose');
+
+// 连接数据库
+mongoose.connect('mongodb://localhost/a', { useNewUrlParser: true });
+
+app.post('/test', function (req, res) {
+  console.log(req.body);
+  console.log(req.query);
+})
+
+// 使用方式  req.session.xxx = xxx
+app.use(session({
+  secret: 'alsdkfj', // 密钥
+  rolling: true, // 每次操作(刷新页面  点击a标签  ajax) 重新设定时间
+  resave: false, // 是否每次请求都重新保存数据
+  saveUninitialized: false, // 初始值
+  cookie: {maxAge: 1000 * 60 * 60},
+  store: new Mongosession({
+    url: 'mongodb://localhost/a'
+  })
+}));
+
+// 获取post参数
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+// 静态资源目录
+app.use(express.static(__dirname + '/public'));
+// 模板引擎
+app.set('views', __dirname + '/view');
+app.set('view engine', 'ejs');
+
+app.use('/', require('./router/index'));
+app.use('/api', require('./router/api'));
+app.use('/admin', require('./router/admin.js'));
+
+app.listen(233);
